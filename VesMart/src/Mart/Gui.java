e797c761eb9bf1e6b4a659e58b2295a60dd13f6d
yashraj <yashrajdesai30@gmail.com>
2020-11-19 17:14:20 +0530

    Initial commit

--- VesMart/src/Mart/Gui.java
+++ VesMart/src/Mart/Gui.java
@@ -3,7 +3,7 @@ package Mart;
 public class Gui {
 
 	public static void main(String[] args) {
-		System.out.println("Hello   ");
+		System.out.println("Hello ");
 
 	}
 
--- gui/src/gui/Mihirgui.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-public class Mihirgui {
-	public static void main(String[] args) {
-		System.out.println("Hello mihir,shreyas,yashraj !!!!!!");
-	}
-}
